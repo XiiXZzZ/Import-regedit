c55d591d6759a1d63caeec5157f9b7ce7fbb21aa
Pieter Goossens <piegoossens@hotmail.com>
2022-09-13 20:56:10 +0200

    Worked out the bugs

--- Essentials.ps1
+++ Essentials.ps1
@@ -3,26 +3,19 @@ Script to change regedit values to personal preferences.
 piegoossens@hotmail.com#>
 
 #Logfile path
-$logfile = [System.Environment]::CurrentDirectory + "\log.txt"
-
-function WriteLog{
-    param([String]$message)
-    $timestamp = (Get-Date).toString("dd/MM/yyyy HH:mm:ss")
-    $logentry = "$timestamp - $message"
-    Add-content $logfile -value $logentry
-}
-
-
+$logfile = pwd
+$logfile = "$logfile\log.txt"
 
 #Set Numlock to always on.
-Set-ItemPropery -Path "HKEY_USERS\.DEFAULT\Control Panel\Keyboard" -Name "InitialKeyboardIndicators" -Value "2"  -PropertyType "String"
+Set-ItemProperty -Path 'Registry::HKEY_USERS\.DEFAULT\Control Panel\Keyboard' -Name "InitialKeyboardIndicators" -Value "2"
 WriteLog "Numlock turned on succesfull"
+
 
 #Disable cortana
 
 
 #Disable sticky keys.
-Set-ItemPropery -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Type String -Value "506"
+Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Type String -Value "506"
 WriteLog "Sticky keys turned off succesfull"
 
 #Switch Windows With a Single Click on the Taskbar.
@@ -49,3 +42,10 @@ WriteLog "Sticky keys turned off succesfull"
 
 #script complete.
 WriteLog "Whole script ran succesfully"
+
+function WriteLog{
+    param ([String]$message)
+    $timestamp = (Get-Date).toString("dd/MM/yyyy HH:mm:ss")
+    $logentry = "$timestamp - $message"
+    Add-content $logfile -value $logentry
+}
